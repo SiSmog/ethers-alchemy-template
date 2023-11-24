5ef66373eb5ede5c7dfa30170e5f858083c87613
SiSmog <majdirekik60@gmail.com>
2023-11-24 12:41:31 +0100

    fix

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 import contract from "./contract.js";
 
-
-const getData=()=>{
-    contract.dernieresDonneesCapteurs().then((res)=>console.log(res))
-}
-const addData=()=>{
-    contract.ajouterDonneesCapteurs(1,10,100).then((res)=>console.log(res))
-}
+contract.methods(...params).then((res)=>console.log(res))
